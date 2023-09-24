8e8a599111c157a9a8459e8c57a9478aab05e2a9
sohel7 <sohelocp@gmail.com>
2023-09-25 00:13:20 +0600

    Update Style.dart

--- lib/Style.dart
+++ lib/Style.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+InputDecoration AppInputDecoration(label){
+  return InputDecoration(
+    contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+    border: OutlineInputBorder(),
+    labelText: label,
+  );
+}
+
+ButtonStyle AppButtonStyle(){
+  return ElevatedButton.styleFrom(
+      padding: EdgeInsets.all(23),
+      backgroundColor: Colors.green,
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(90))
+
+      )
+  );
+}
+
+
+SizedBox SizeBox50(child){
+  return SizedBox(
+    width: double.infinity,
+    child: Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(8),
+      child: child,
+    ),
+  );
+}
