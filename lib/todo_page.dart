27a4250b275227f197396d0e16bc85d711f1b124
sohel7 <sohelocp@gmail.com>
2023-09-25 17:10:38 +0600

    Update todo_page.dart

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -7,7 +7,7 @@ class ToDoPage extends StatefulWidget{
 
   @override
   State<StatefulWidget> createState() {
-  return ToDoPageView();
+    return ToDoPageView();
 
   }
 }
@@ -16,7 +16,7 @@ class ToDoPageView  extends State<ToDoPage>{
 
   List ToDOList= [];
 
- String item="";
+  String item="";
 
   MyInputOnChange (content){
     setState(() {
@@ -30,54 +30,58 @@ class ToDoPageView  extends State<ToDoPage>{
     });
   }
 
-
+  RemoveItem(index){
+    setState(() {
+      ToDOList.removeAt(index);
+    });
+  }
 
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(title: Text('TO Do'),),
-    body: Container(
-       padding: EdgeInsets.all(10),
-      child: Column(
-        children: [
+    return Scaffold(
+      appBar: AppBar(title: Text('TO Do'),),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
             Expanded(
-              flex: 10,
+                flex: 10,
                 child: Row(
                   children: [
                     Expanded(
-                      flex: 70,
+                        flex: 70,
                         child: TextFormField(onChanged: (content){MyInputOnChange(content);}   ,decoration: AppInputDecoration("List Item"),)),
                     Expanded(flex: 20, child: Padding(padding: EdgeInsets.only(left: 5),
-                    child:  ElevatedButton(onPressed: (){AddItem();},child: Text("Add"),style: AppButtonStyle(),),
+                      child:  ElevatedButton(onPressed: (){AddItem();},child: Text("Add"),style: AppButtonStyle(),),
                     )
                     )
                   ],
                 )
             ),
             Expanded(
-              flex: 90,
+                flex: 90,
                 child: ListView.builder(
-                  itemCount: ToDOList.length,
+                    itemCount: ToDOList.length,
                     itemBuilder: (context,index){
-                    return Card(
-                      child: SizeBox50(
-                        Row(
-                          children: [
-                            Expanded(flex: 80, child: Text(ToDOList[index]['item'].toString())),
-                            Expanded(flex: 20, child: TextButton(onPressed: (){},child: Icon(Icons.delete),))
-                          ],
-                        )
-                      )
-                    );
+                      return Card(
+                          child: SizeBox50(
+                              Row(
+                                children: [
+                                  Expanded(flex: 80, child: Text(ToDOList[index]['item'].toString())),
+                                  Expanded(flex: 20, child: TextButton(onPressed: (){RemoveItem(index);},child: Icon(Icons.delete),))
+                                ],
+                              )
+                          )
+                      );
                     }
                 )
             )
-        ],
+          ],
+        ),
       ),
-    ),
 
-   );
+    );
   }
 
 }
